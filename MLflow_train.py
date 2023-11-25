cab6ef79f3a8f86f40e85b3e4210e7adea1b62fb
Than Than Swe <thanthanswe124509@gmail.com>
2023-11-26 02:24:07 +0900

    ML flow first commit changes

--- MLflow_train.py
+++ MLflow_train.py
@@ -75,6 +75,10 @@ if __name__ == "__main__":
         mlflow.log_metric("r2", r2)
         mlflow.log_metric("mae", mae)
 
+        #For remote server only (Dagshub)
+
+        remote_server_url = https://dagshub.com/thanthanswe-github/Classfication_Model_Using_MLflow.mlflow
+        mlflow.set_tracking_url(remote_server_url)
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
         # Model registry does not work with file store
