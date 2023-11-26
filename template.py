b0f311a855d5150d97236bd146f22970da307465
Than Than Swe <thanthanswe124509@gmail.com>
2023-11-26 19:54:09 +0900

    added template file

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name ="CNN_Classifier_Project"
+project_name ="CNNClassifier_Project"
 
 ##
 list_of_files = [
